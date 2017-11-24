1429b03ce04fd26880b7a0011d447ed5d8c42da1
andreshp <andreshp9@gmail.com>
2017-11-24 10:25:20 +0100

    updates emacs

--- init.el
+++ init.el
@@ -36,3 +36,21 @@
 
 ;; Adds the code found in myinit.org to emacs configuration.
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(font-use-system-font t)
+ '(package-selected-packages
+   (quote
+    (sage-shell-mode visual-fill-column use-package solarized-theme smart-tabs-mode pdf-tools parsebib paradox org-gnome org-gcal org-bullets org-beautify-theme nyan-mode multiple-cursors multi-term material-theme magit magic-latex-buffer linum-relative key-chord gap-mode f elpy cdlatex calfw-gcal calfw biblio better-defaults auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 102 :width normal :foundry "PfEd" :family "Source Code Pro")))))
